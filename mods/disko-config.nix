b16dc1ec16eff403d644f27e764c0ff3c0a7c394
Jack Rosenberg <github@jackr.eu>
2025-11-30 20:42:39 +0100

    zfs try 2

--- mods/disko-config.nix
+++ mods/disko-config.nix
@@ -4,19 +4,15 @@
 #  imports = [ ./disko-config.nix ];
 #  disko.devices.disk.main.device = "/dev/sda";
 # }
-{ pkgs, config, ...}:
 {
   disko.devices = {
     disk = {
       main = {
+        device = "/dev/nvme0n1";
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
-            # boot = { # WHAT DOES THIS DO???
-            #   size = "1M";
-            #   type = "EF02"; # for grub MBR
-            # };
             ESP = {
               size = "1G";
               type = "EF00";
@@ -24,7 +20,7 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
+                mountOptions = [ "nofail" "umask=0077"];
               };
             };
             zfs = {
@@ -42,36 +38,15 @@
       zroot = {
         type = "zpool";
         rootFsOptions = {
+          mountpoint = "none";
           compression = "zstd";
           "com.sun:auto-snapshot" = "false";
         };
+        options.ashift = "12";
         postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank"; # create a blank snapshot on creation
 
         # datasets config
         datasets = {
-          # for all the stuff i want to save
-          persist = {
-            type = "zfs_fs";
-            mountpoint = "/persist";
-            options = {
-              "com.sun:auto-snapshot" = "true";
-              encryption = "aes-256-gcm";
-              mountpoint = "legacy";
-            };
-          };
-          # for /nix/{store,var} Needs to persist as well
-          nix = {
-            type = "zfs_fs";
-            options.mountpoint = "legacy";
-            mountpoint = "/nix";
-            options = {
-              encryption = "aes-256-gcm";
-              atime = "off";
-              canmount = "on";
-              "com.sun:auto-snapshot" = "false";
-            };
-            postCreateHook = "zfs snapshot zroot/nix@empty";
-          };
           # Where everything else lives, and is wiped on reboot by restoring a blank zfs snapshot.
           root = {
             type = "zfs_fs";
@@ -81,9 +56,29 @@
               mountpoint = "legacy";
               "com.sun:auto-snapshot" = "false";
             };
-            postCreateHook = ''
-                zfs snapshot zroot/root@empty
-            '';
+            postCreateHook = "zfs snapshot zroot/root@empty";
+          };
+          # for all the stuff i want to save
+          "root/persist" = {
+            type = "zfs_fs";
+            mountpoint = "/persist";
+            options = {
+              "com.sun:auto-snapshot" = "true";
+              encryption = "aes-256-gcm";
+              mountpoint = "/persist";
+            };
+          };
+          # for /nix/{store,var} Needs to persist as well
+          "root/nix" = {
+            type = "zfs_fs";
+            options.mountpoint = "/nix";
+            mountpoint = "/nix";
+            options = {
+              encryption = "aes-256-gcm";
+              atime = "off";
+              "com.sun:auto-snapshot" = "false";
+            };
+            postCreateHook = "zfs snapshot zroot/nix@empty";
           };
         };
       };
@@ -92,10 +87,8 @@
   # zfs zpool config
   # extra zfs config to make it work
    networking.hostId = "01330c81"; #head -c4 /dev/urandom | od -A none -t x4
-   environment.systemPackages = [pkgs.zfs-prune-snapshots];
    boot = {
      # Newest kernels might not be supported by ZFS
-     kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
      kernelParams = [
        "nohibernate"
        "zfs.zfs_arc_max=17179869184"
