722561e7fc4356c119355100dcac62c979c6dfe7
jack rosenberg <mail@jackr.eu>
2025-12-29 23:55:17 +0100

    agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,17 +1,18 @@
 let
-  system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP0FUN22vJEZ5ByeUms4KvXfYe9g4UnAlaVMmhr5cO8/";
+  laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP0FUN22vJEZ5ByeUms4KvXfYe9g4UnAlaVMmhr5cO8/";
+  server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKRegupPNZScgm0WszmUGt3H4o3S1SIUUX8zjjRmC6pp";
 in
 {
-  "rcloneConf.age".publicKeys = [ system1 ];
-  "resticPDrivePass.age".publicKeys = [ system1 ];
-  "transmissionCreds.age".publicKeys = [ system1 ];
-  "healthchecksSettings.age".publicKeys = [ system1 ];
-  "newtConf.age".publicKeys = [ system1 ];
-  "lasuite.age".publicKeys = [ system1 ];
-  "garageRpcSecret.age".publicKeys = [ system1 ];
-  "garageAdminToken.age".publicKeys = [ system1 ];
-  "gitlab.age".publicKeys = [ system1 ];
-  "vpn.ovpn.age".publicKeys = [ system1 ];
-  "vpn-auth-user-pass.age".publicKeys = [ system1 ];
-  "dexsecret.age".publicKeys = [ system1 ];
+  "rcloneConf.age".publicKeys = [ laptop server ];
+  "resticPDrivePass.age".publicKeys = [ laptop server ];
+  "transmissionCreds.age".publicKeys = [ laptop server ];
+  "healthchecksSettings.age".publicKeys = [ laptop server ];
+  "newtConf.age".publicKeys = [ laptop server ];
+  "lasuite.age".publicKeys = [ laptop server ];
+  "garageRpcSecret.age".publicKeys = [ laptop server ];
+  "garageAdminToken.age".publicKeys = [ laptop server ];
+  "gitlab.age".publicKeys = [ laptop server ];
+  "vpn.ovpn.age".publicKeys = [ laptop server ];
+  "vpn-auth-user-pass.age".publicKeys = [ laptop server ];
+  "dexsecret.age".publicKeys = [ laptop server ];
 }
